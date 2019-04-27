6df1cdfe62b671e65565676e6193dc6b1fbe7989
letthewookieewin <sean@seancox.ca>
2019-04-26 17:33:18 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5e1e56cee3e6690116a0.js"
+    "url": "webpack-runtime-b2c0533a899e7ff4e3e0.js"
   },
   {
     "url": "app-a60d48b81b82eab633eb.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6034951ce4f7cb2a968bc0fb7bbc3971"
+    "revision": "48c54f904986c25c6211d69c9250c4c4"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
